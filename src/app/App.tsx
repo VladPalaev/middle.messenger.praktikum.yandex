ec9cb33058445e580d3a18bd82fef96a6413ba31
Vladislav <vladislav.palaev@yandex.ru>
2023-01-26 01:41:27 +0300

    feat(app/app.tsx): добавил новые роуты
    Есть новые представления страниц Error404 и Error 500

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import React from "mine-react";
 import { AuthPage } from "./pages/auth/AuthPage"; //todo потом подлючить через Router
 import { ChatPage } from "./pages/chat/ChatPage";
 import { ProfilePage } from "./pages/profile/ProfilePage";
+import { ErrorPage } from "./pages/error/ErrorPage";
 
 export const App = () => {
 	return (
@@ -9,6 +10,14 @@ export const App = () => {
 			<AuthPage />
 			<ChatPage />
 			<ProfilePage />
+			<ErrorPage
+				statusCode="500"
+				messageError="Ошибка на стороне сервера"
+			/>
+			<ErrorPage
+				statusCode="404"
+				messageError="Проверьте подключение к интернету"
+			/>
 		</>
 	);
 };
